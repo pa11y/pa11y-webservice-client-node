0d9f0763643f2c72e90a099d88570df8f18b099d
Danyal Aytekin <danyal@fubase.com>
2025-11-16 21:56:31 +0000

    Upgrade to ESLint 9 and `eslint-config-pa11y`

---
+++ eslint.config.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const {defineConfig} = require('eslint/config');
+
+const configPa11y = require('eslint-config-pa11y');
+
+module.exports = defineConfig([
+	configPa11y,
+	{
+		rules: {
+			'n/no-unsupported-features/node-builtins': ['error', {
+				ignores: ['fetch']
+			}]
+		}
+	}
+]);
